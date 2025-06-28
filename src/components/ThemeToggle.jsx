e702c67af995b2228996595f541b89e334b9da4f
Sheri Nawaz <sherinawaz495@gmail.com>
2025-06-29 00:36:48 +0100

    Update ThemeToggle.jsx

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 import { cn } from '@/lib/utils'
 
 export const ThemeToggle = () => {
-    const [isDarkMove, setIsDarkMode] = useState(false);
+    const [isDarkMove, setIsDarkMode] = useState(true);
 
     useEffect(() => {
         const storedTheme = localStorage.getItem("theme")
